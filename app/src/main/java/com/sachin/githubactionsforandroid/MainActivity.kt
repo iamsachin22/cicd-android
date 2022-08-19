98b66d7f8c5ae56fd5c8974b190c6ae0f402e651
sachinshelar <sachinshelar1122@gmail.com>
2022-08-19 11:55:54 +0530

    intial commit

--- app/src/main/java/com/sachin/githubactionsforandroid/MainActivity.kt
+++ app/src/main/java/com/sachin/githubactionsforandroid/MainActivity.kt
@@ -3,11 +3,13 @@ package com.sachin.githubactionsforandroid
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -32,7 +34,9 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!", textAlign = TextAlign.Center)
+    Box(contentAlignment = Alignment.Center,) {
+        Text(text = "Hello $name!", textAlign = TextAlign.Center)
+    }
 }
 
 @Preview(showBackground = true)
